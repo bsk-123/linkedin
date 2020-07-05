3742b595332a4f6c08fd75e87ee43e8283788ba3
bsk-123 <43913935+bsk-123@users.noreply.github.com>
2020-07-05 21:25:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector(".see").addEventListener("click",function(){
+    document.querySelector(".mc").style.display="inline";
+    document.querySelector(".see").style.display="none";
+})
