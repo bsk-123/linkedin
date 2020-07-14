d4306e1bce707c4a8f278ce442ee2e6daaf91ef7
bsk-123 <43913935+bsk-123@users.noreply.github.com>
2020-07-14 15:01:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ document.querySelector(".see").addEventListener("click",function(){
     document.querySelector(".mc").style.display="inline";
     document.querySelector(".see").style.display="none";
 })
+var click=0
+document.querySelector(".ham").addEventListener("click",function(){
+    click+=1
+    console.log(click)
+    if(click%2!=0)
+    document.querySelector(".ham-menu").style.transform = "translateX(0)" ;
+    else
+    document.querySelector(".ham-menu").style.transform = "translatex(-500px)"
+})
